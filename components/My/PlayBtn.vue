7506bf121d69d95f3cf67a6450fff52b2a824f52
johnkaa <g.johnka@gmail.com>
2022-01-26 13:20:46 +0200

    add play btn

---
+++ components/My/PlayBtn.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="play-icon" @click="play"></div>
+</template>
+
+<script>
+export default {
+  methods: {
+    play() {
+      this.$emit('play')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .play-icon {
+    width: 61px;
+    height: 61px;
+    border: 2px solid #fff;
+    border-radius: 50%;
+    cursor: pointer;
+    position: relative;
+    transition: all .5s;
+    &::after {
+      content: ''; 
+      position: absolute; /* Абсолютное позиционирование */
+      top: 50%;
+      left: 60%;
+      transform: translate(-50%, -50%);
+      border: 8px solid transparent; /* Прозрачные границы */
+      border-left: 10px solid #fff; /* Добавляем треугольник */
+    }
+    &:hover {
+      border-color: #337D5A;
+      background-color: #337D5A;
+    }
+  }
+</style>
