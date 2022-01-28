f78dca2ad7b20716e2def6097bc9e40f7f17d53a
johnkaa <g.johnka@gmail.com>
2022-01-28 17:03:17 +0200

    add user in vuex storage

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -49,7 +49,8 @@ export default {
       try {
         await this.$fire.auth
           .signInWithEmailAndPassword(this.email, this.password)
-          .then(() => {
+          .then((res) => {
+            this.$store.dispatch('getUserAction', res.user.uid)
             this.$router.push('/')
             this.$toasted.success('Вы успешно вошли в аккаунт.')
           })
