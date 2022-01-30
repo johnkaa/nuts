4313b8233b54b4fa7734c288731cc1d5e581348d
johnkaa <g.johnka@gmail.com>
2022-01-30 15:58:28 +0200

    add gallery card component template

---
+++ components/GalleryCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="gallery-card">
+    <img class="gallery-card__img" src="/images/gallery-1.jpg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.gallery-card {
+  max-width: 425px;
+  &__img {
+    max-width: 100%;
+  }
+}
+</style>
