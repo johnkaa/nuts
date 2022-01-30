a6ead5353104d114e64bee5cfe755d7eb0357b75
johnkaa <g.johnka@gmail.com>
2022-01-30 13:07:45 +0200

    add about component template

---
+++ components/About.vue
@@ -0,0 +1,76 @@
+<template>
+      <div class="about">
+      <div class="container">
+        <div class="about__inner">
+          <div class="about__info">
+            <h2 class="about__title">О производителе</h2>
+            <p class="about__text">
+              Сельскохозяйственный обслуживающий кооператив “Орех Причерноморья”
+              выращивает смешанные сады ореха грецкого с фундуком, а также ореха
+              грецкого с шиповником, на площади более 150 гектаров. Помимо этого
+              кооператив занимается выращиванием посадочного материала, саженцов
+              привитого и непривитого грецкого ореха, фундука и шиповника.
+            </p>
+            <nuxt-link class="about__link" to="/about">
+              <my-button class="about__btn secondary">Подробнее</my-button>
+            </nuxt-link>
+          </div>
+          <div class="about__slider">
+            <img src="/images/about-slider-img.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.about {
+  padding: 90px 0;
+  background-color: #fbfbfb;
+  &__inner {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  &__title {
+    font-size: 45px;
+    font-weight: 800;
+    margin-bottom: 40px;
+  }
+  &__text {
+    margin-bottom: 40px;
+    max-width: 480px;
+  }
+  &__btn {
+    max-width: 170px;
+  }
+  &__slider {
+    max-width: 700px;
+    img {
+      width: 100%;
+    }
+  }
+}
+@media (max-width: 1300px) {
+  .about {
+    &__inner {
+      flex-wrap: wrap;
+      justify-content: center;
+      text-align: center;
+      gap: 50px;
+    }
+    &__title {
+      font-size: 38px;
+    }
+    &__btn {
+      max-width: 300px;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -19,28 +19,7 @@
         </nuxt-link>
       </div>
     </div>
-    <div class="about">
-      <div class="container">
-        <div class="about__inner">
-          <div class="about__info">
-            <h2 class="about__title">О производителе</h2>
-            <p class="about__text">
-              Сельскохозяйственный обслуживающий кооператив “Орех Причерноморья”
-              выращивает смешанные сады ореха грецкого с фундуком, а также ореха
-              грецкого с шиповником, на площади более 150 гектаров. Помимо этого
-              кооператив занимается выращиванием посадочного материала, саженцов
-              привитого и непривитого грецкого ореха, фундука и шиповника.
-            </p>
-            <nuxt-link class="about__link" to="/about">
-              <my-button class="about__btn secondary">Подробнее</my-button>
-            </nuxt-link>
-          </div>
-          <div class="about__slider">
-            <img src="/images/about-slider-img.jpg" alt="" />
-          </div>
-        </div>
-      </div>
-    </div>
+    <About class="about"/>
     <banners-area class="area" />
     <banners-factory class="factory" />
     <div class="benefits">
@@ -126,33 +105,6 @@ export default {}
     position: relative;
     left: 50%;
     transform: translateX(-50%);
-  }
-}
-.about {
-  padding: 90px 0;
-  background-color: #fbfbfb;
-  &__inner {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-  &__title {
-    font-size: 45px;
-    font-weight: 800;
-    margin-bottom: 40px;
-  }
-  &__text {
-    margin-bottom: 40px;
-    max-width: 480px;
-  }
-  &__btn {
-    max-width: 170px;
-  }
-  &__slider {
-    max-width: 700px;
-    img {
-      width: 100%;
-    }
   }
 }
 .benefits {
@@ -242,23 +194,7 @@ export default {}
     }
   }
 }
-@media (max-width: 1300px) {
-  .about {
-    &__inner {
-      flex-wrap: wrap;
-      justify-content: center;
-      text-align: center;
-      gap: 50px;
-    }
-    &__btn {
-      max-width: 300px;
-    }
-  }
-}
 @media (max-width: 440px) {
-  .about__title {
-    font-size: 38px;
-  }
   .benefits {
     &__title {
       font-size: 34px;
