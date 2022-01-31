ac9df5ced8559f141e70a140f8cb597bb5be32e5
johnkaa <g.johnka@gmail.com>
2022-01-31 17:23:07 +0200

    add first news card styles

--- components/News/Card.vue
+++ components/News/Card.vue
@@ -1,31 +1,33 @@
 <template>
   <div class="news-card">
     <img class="news-card__img" src="/images/news-1.jpg" />
-    <div class="news-card__date">12.04.2019</div>
-    <div class="news-card__title">
-      Центр поддержки экспорта, в студии Павел Тулба
+    <div class="news-card__inner">
+      <div class="news-card__date">12.04.2019</div>
+      <div class="news-card__title">
+        Центр поддержки экспорта, в студии Павел Тулба
+      </div>
+      <div class="news-card__text">
+        Как уже упоминал Павел Тулба, шиповник цветёт. Эта культура очень
+        недооценена среди наших садоводов, не смотря на большой спрос в Европе и
+        отличную...
+      </div>
+      <nuxt-link class="news-card__link" to="/news/1">
+        Читать
+        <svg
+          width="14"
+          height="14"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            class="news-card__link-arrow"
+            d="M7.00001 11L6.3 10.3L10.1 6.5H0V5.50002H10.1L6.3 1.7L7.00001 1.00002L12 6.00001L7.00001 11Z"
+            fill="#1A2F3F"
+          />
+        </svg>
+      </nuxt-link>
     </div>
-    <div class="news-card__text">
-      Как уже упоминал Павел Тулба, шиповник цветёт. Эта культура очень
-      недооценена среди наших садоводов, не смотря на большой спрос в Европе и
-      отличную...
-    </div>
-    <nuxt-link class="news-card__link" to="/news/1">
-      Читать
-      <svg
-        width="14"
-        height="14"
-        viewBox="0 0 12 12"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          class="news-card__link-arrow"
-          d="M7.00001 11L6.3 10.3L10.1 6.5H0V5.50002H10.1L6.3 1.7L7.00001 1.00002L12 6.00001L7.00001 11Z"
-          fill="#1A2F3F"
-        />
-      </svg>
-    </nuxt-link>
   </div>
 </template>
 
@@ -42,7 +44,10 @@ export default {}
     margin-bottom: 20px;
     width: 100%;
   }
-  &__date, &__title, &__text, &__link {
+  &__date,
+  &__title,
+  &__text,
+  &__link {
     padding-left: 24px;
   }
   &__date {
@@ -69,15 +74,37 @@ export default {}
     display: flex;
     align-items: center;
     gap: 8px;
-    transition: color .3s;
+    transition: color 0.3s;
     &-arrow {
-      transition: fill .3s;
+      transition: fill 0.3s;
     }
     &:hover {
-      color: #337D5A;
+      color: #337d5a;
       path {
-        fill: #337D5A;
+        fill: #337d5a;
       }
+    }
+  }
+  &.big {
+    display: flex;
+    gap: 50px;
+    max-width: 100%;
+    .news-card {
+      &__img {
+        max-width: 850px;
+      }
+      &__text {
+        max-width: 400px;
+      }
+    }
+  }
+}
+@media (max-width: 1335px) {
+  .news-card.big {
+    max-width: 408px;
+    display: block;
+    .news-card__text {
+      max-width: 350px;
     }
   }
 }
