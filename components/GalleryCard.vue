fb5e9cc4eddb6dbf44f0b835b4a91f8d33c56e94
johnkaa <g.johnka@gmail.com>
2022-02-01 22:27:54 +0200

    add title and text in card

--- components/GalleryCard.vue
+++ components/GalleryCard.vue
@@ -20,6 +20,10 @@
       class="gallery-card__play"
       @play="showVideo"
     />
+    <div v-if="galleryCard.title || galleryCard.text" class="gallery-card__content" :class="{ video: galleryCard.video, img: !galleryCard.video }">
+      <div class="gallery-card__title">{{ galleryCard.title.slice(0, 15) }}<span v-if="galleryCard.title.length > 15">...</span></div>
+      <p class="gallery-card__text">{{ galleryCard.text }}</p>
+    </div>
   </div>
 </template>
 
@@ -53,6 +57,59 @@ export default {
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
+    z-index: 10;
+  }
+  &__content {
+    font-family: 'Montserrat', sans-serif;
+    position: absolute;
+    color: #fff;
+    padding: 24px;
+    right: 0;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    .gallery-card {
+      &__title {
+        font-size: 22px;
+        font-weight: 800;
+        width: 100%;
+        margin-bottom: 20px;
+      }
+      &__text {
+        font-size: 14px;
+        line-height: 21px;
+        width: 80%;
+      }
+    }
+    &.img {
+      background-color: #337D5A;
+      opacity: 0;
+      cursor: pointer;
+      transition: opacity .3s;
+      &:hover {
+        opacity: 1;
+      }
+    }
+    &.video {
+      text-align: center;
+      background-color: rgba(#000, .3);
+      .gallery-card {
+        &__title, &__text {
+          position: absolute;
+          left: 50%;
+          transform: translateX(-50%);
+        }
+        &__title {
+          top: 65%;
+        }
+        &__text {
+          top: 75%;
+        }
+      }
+    }
+    &.text {
+
+    }
   }
 }
 </style>
