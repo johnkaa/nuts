819e83b68e569236821b3ac4a1a7b1ccdff58fa6
johnkaa <g.johnka@gmail.com>
2022-02-21 22:30:40 +0200

    fix auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ import { getAuth, onAuthStateChanged } from 'firebase/auth'
 export default ({ redirect }) => {
   const auth = getAuth()
     onAuthStateChanged(auth, (user) => {
-      if(!user) {
+      if(user !== null) {
         redirect('/')
       }
     })
