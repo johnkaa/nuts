f0bad7dee349ac8c2364f0d7bf707093cf70b12a
johnkaa <g.johnka@gmail.com>
2022-02-21 20:28:54 +0200

    fix cabinet security

--- middleware/cabinet.js
+++ middleware/cabinet.js
@@ -1,10 +1,15 @@
-export default ({ app, route, redirect }) => {
-  // const cabinetId = route.params.id
-  // let userId = null
-  // if(app.store.state.user) {
-  //   userId = app.store.state.user.uid
-  // }
-  // if(cabinetId !== userId) {
-  //   redirect('/')
-  // }
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
+export default ({ route, redirect }) => {
+  const cabinetId = route.params.id
+  const auth = getAuth()
+    onAuthStateChanged(auth, (user) => {
+      if(user) {
+        if (user.uid !== cabinetId) {
+          redirect('/')
+        }
+      } else {
+        redirect('/')
+      }
+    })
 }
